ba9c4998c32dd322df4bc9d098caf96c5c6c4dd2
faizal108 <69467378+faizal108@users.noreply.github.com>
2024-07-04 19:01:56 +0530

    init

---
+++ src/test/java/com/faizal108/Scalable_Book_Api/ScalableBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizal108.Scalable_Book_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
