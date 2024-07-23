d260dc45f400daddfe38b190276f8465da8ffe01
faizal108 <69467378+faizal108@users.noreply.github.com>
2024-07-23 18:26:12 +0530

    Eureka Server Implemented

--- bookService/src/main/java/com/faizal108/bookService/BookServiceApplication.java
+++ bookService/src/main/java/com/faizal108/bookService/BookServiceApplication.java
@@ -3,11 +3,13 @@ package com.faizal108.bookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EntityScan("com.faizal108.commanEntity")
 @EnableScheduling
+@EnableDiscoveryClient
+@EntityScan("com.faizal108.commanEntity")
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
